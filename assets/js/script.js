01f96d62ba6c58c8d8793da8fe8b6cecdea18906
cottonatocd <cotton@ocdagency.com>
2020-07-01 16:11:47 -0400

    tiny bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ function calcTime(offset) {
     utc = d.getTime() + (d.getTimezoneOffset() * 60000);
     nd = new Date(utc + (3600000*offset));
     var ampm = (nd.getHours() >= 12) ? "PM" : "AM";
-    // console.log(nd.getHours()%12, nd.getMinutes());
-    
-    return nd.getHours()%12 + "<span class='colon'>:</span>" + ("0" + nd.getMinutes()).slice(-2) + " " + ampm;
+    // console.log(nd.getHours()%12, h);
+    var h = (nd.getHours()%12 == 0) ? 12 : nd.getHours()%12;
+    return h + "<span class='colon'>:</span>" + ("0" + nd.getMinutes()).slice(-2) + " " + ampm;
 }
 
 document.getElementById("timeKC").innerHTML = calcTime('-5');
